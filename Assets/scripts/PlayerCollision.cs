d7b755191c22f5d79fc888ab4e89cd316bf5649c
Daniel Fairley <poemdexter@gmail.com>
2014-01-25 17:21:58 -0600

    collisions, player movement, first song

---
+++ Assets/scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // on hit rat, die die die
+        if (col.gameObject.CompareTag("Rat")) {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        Debug.Log("Respawn");
+    }
+}
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -3,13 +3,10 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
-	
-    }
-    
+    public float speed;
+     
     void Update()
     {
-    
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal"), 0) * speed * Time.deltaTime); 
     }
 }
