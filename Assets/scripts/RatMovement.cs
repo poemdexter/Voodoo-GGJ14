7636eb7b36a30ba36b8795d57fa4c8c14e342271
poemdexter <poemdexter@gmail.com>
2014-01-25 11:44:32 -0600

    init commit

---
+++ Assets/scripts/RatMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RatMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float currentTime = 0;
+    public float switchTime = 2.0f;
+    
+    void Update()
+    {
+        if ((currentTime += Time.deltaTime) > switchTime) {
+            speed = -speed;
+            currentTime = 0;
+        }
+        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
