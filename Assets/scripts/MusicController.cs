92185787cf8a0c83084680a230a56b96e55b3ed8
poemdexter <poemdexter@gmail.com>
2014-01-26 16:00:21 -0600

    done!

--- Assets/scripts/MusicController.cs
+++ Assets/scripts/MusicController.cs
@@ -6,10 +6,21 @@ public class MusicController : MonoBehaviour
     public AudioSource musicIntro;
     public AudioSource musicLoop;
     
+    bool stopped = false;
+    
     void Update()
     {
-        if (!musicIntro.isPlaying && !musicLoop.isPlaying) {
-            musicLoop.Play();
+        if (!stopped) {
+            if (!musicIntro.isPlaying && !musicLoop.isPlaying) {
+                musicLoop.Play();
+            }
         }
+    }
+    
+    public void StopMusic()
+    {
+        stopped = true;
+        musicLoop.Stop();
+        musicIntro.Stop();
     }
 }
---
+++ Assets/scripts/PlayWinMovie.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayWinMovie : MonoBehaviour
+{
+    public bool triggered = false;
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!triggered && col.CompareTag("Player")) {
+            triggered = true;
+            col.gameObject.GetComponent<PlayerMovement>().enabled = false;
+            col.gameObject.GetComponent<JumpPhysics2D>().enabled = false;
+            Camera.main.GetComponent<MusicController>().StopMusic();
+            transform.position = new Vector3(transform.position.x, transform.position.y, -0.1f);
+            renderer.enabled = true;
+            ((MovieTexture)(renderer.material.mainTexture)).Play();
+            audio.Play();
+        }
+    }
+}
